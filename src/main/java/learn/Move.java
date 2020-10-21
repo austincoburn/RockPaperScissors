d6c15aff5f31ec4ee8e41a054fc0f305899f3b00
charIiemurphy <38874273+charIiemurphy@users.noreply.github.com>
2020-10-21 14:26:54 -0500

    added to Rule

--- src/main/java/learn/Move.java
+++ src/main/java/learn/Move.java
@@ -12,4 +12,6 @@ public enum Move {
     public int getMoveValue() {
         return moveValue;
     }
+
+
 }
--- src/main/java/learn/Rule.java
+++ src/main/java/learn/Rule.java
@@ -1,8 +1,12 @@
 package learn;
 
+import java.util.Random;
+
 public class Rule {
     private int userScore = 0;
     private int computerScore = 0;
+    private int currentRound = 0;
+    private int maxRounds = 3;
 
     public int getUserScore() {
         return userScore;
@@ -11,4 +15,52 @@ public class Rule {
     public int getComputerScore() {
         return computerScore;
     }
+
+    public Move generateComputerMove() {
+        Random random = new Random();
+        return Move.values()[random.nextInt(3) + 1];
+    }
+
+    public String compare(String userMoveStr) {
+        currentRound++;
+        userMoveStr = userMoveStr.toUpperCase();
+        Move userMove;
+
+        userMove = Move.valueOf(userMoveStr);
+        Move compMove = generateComputerMove();
+
+        switch(userMove) {
+            case ROCK:
+                if(compMove == Move.PAPER) {
+                    computerScore++;
+                    return "Lost";
+                } else if (compMove == Move.SCISSORS) {
+                    userScore++;
+                    return "Win";
+                }
+                break;
+            case PAPER:
+                if(compMove == Move.SCISSORS) {
+                    computerScore++;
+                    return "Lost";
+                } else if (compMove == Move.ROCK) {
+                    userScore++;
+                    return "Win";
+                }
+                break;
+            case SCISSORS:
+                if(compMove == Move.ROCK) {
+                    computerScore++;
+                    return "Lost";
+                } else if (compMove == Move.PAPER) {
+                    userScore++;
+                    return "Win";
+                }
+                break;
+            default:
+                return "Not a valid move";
+        }
+    return "Tie";
+    }
+
 }
