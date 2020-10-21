9131515ddb5bc902cfdeb7cfc7040571d275a65f
Austin Coburn <austincoburn41@gmail.com>
2020-10-21 13:48:37 -0500

    added rule class

---
+++ src/main/java/learn/Move.java
@@ -0,0 +1,15 @@
+package learn;
+
+public enum Move {
+    ROCK(1), PAPER(2), SCISSORS(3);
+
+    private int moveValue;
+
+    Move(int moveValue) {
+        this.moveValue = moveValue;
+    }
+
+    public int getMoveValue() {
+        return moveValue;
+    }
+}
---
+++ src/main/java/learn/Rule.java
@@ -0,0 +1,14 @@
+package learn;
+
+public class Rule {
+    private int userScore = 0;
+    private int computerScore = 0;
+
+    public int getUserScore() {
+        return userScore;
+    }
+
+    public int getComputerScore() {
+        return computerScore;
+    }
+}
